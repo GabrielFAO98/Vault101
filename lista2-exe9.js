2d6c52282d286d986e7875e7f4d64cfdc177a4a9
GabrielFAO98 <gfdaoliveira@gmail.com>
2020-08-26 22:02:34 -0300

    exe9 versao final

--- lista2-exe9.js
+++ lista2-exe9.js
@@ -9,6 +9,13 @@
     }
 
 
-    function exe9(num){
-        
-    }
+    function exe9(num){ //supondo num = 6
+
+        var soma = 0
+        for(var divisor=num: divisor >=1; divisor==){
+            if (num % divisor == 0){
+                soma - soma + divisor
+            }
+        }
+        console.log(`A soma dos divisores de ${num} é ${soma}`)
+    } 
